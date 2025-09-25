091411883daf560e9a0bd6197f601371c9d34d43
Felipe Flores <felipe@felipeflores.io>
2025-09-25 16:01:28 -0300

    Change libsForQt5 with kdePackages

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -2,7 +2,7 @@
   props = builtins.fromJSON (builtins.readFile ../props.json);
 in {
   sddm-sugar-candy-nix = final: prev: {
-    sddm-sugar-candy-nix = final.libsForQt5.callPackage ./default.nix {
+    sddm-sugar-candy-nix = final.kdePackages.callPackage ./default.nix {
       version = props.version;
     };
   };
